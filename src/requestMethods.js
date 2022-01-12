0136a86441cf85b8a299caa0cf6870dc69f0bfd2
Basudev <murabasudev@gmail.com>
2022-01-12 08:34:50 +0530

    localhost port changed

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3000/api/v1/";
+const BASE_URL = "http://localhost:5000/api/v1/";
 
 // const Token = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
 //   .currentUser.token;
